6a7ed80ef1ee7a746549e2e5cbf7697a7187df3c
Ashley <ashley_narcisseaustin@ultimatesoftware.com>
2019-07-11 22:57:10 -0400

    fix(secrets): removed duplicate test and and added scope for improved deployment

--- __test__/messenger.spec.js
+++ __test__/messenger.spec.js
@@ -13,42 +13,28 @@ describe('mesenger', () => {
         const name = msg[0].text.text.match(/(<@)\w+>/g).pop();
         const testmsg = msg[1].message.includes('test');
 
-        console.log
         expect(name).toBeTruthy();
         expect(testmsg).toBe(true);
       });
-    })
-  
-    describe('should ommit greeting', () => {
-      test('when a user id is missing or falsy', () => {
-        const msg = greeting(0, []);
-        const msg1 = greeting('', []);
-        const msg2 = greeting(null, []);
-        const msg3 = greeting(undefined, []);
-  
-        expect(msg.length).toBe(0);
-        expect(msg1.length).toBe(0);
-        expect(msg2.length).toBe(0);
-        expect(msg3.length).toBe(0);
-      });
     });
 
-    describe('should format', () => {
-      test('should properly format @user mention', () => {
-        const id = `1234EF`; 
-        const msg = greeting(id, []);
-        const name = msg[0].text.text.match(/(<@)\w+>/g).pop();
-  
-        expect(name).toBe('<@1234EF>');
-      });
-  
-      test('should correct user ID and format @user mention', () => {
-        const id = `1234ef`; 
-        const msg = greeting(id, []);
-        const name = msg[0].text.text.match(/(<@)\w+>/g).pop();
-  
-        expect(name).toBe('<@1234EF>');
-      });
-    })
+    test('should ommit greeting when a user id is missing or falsy', () => {
+      const msg = greeting(0, []);
+      const msg1 = greeting('', []);
+      const msg2 = greeting(null, []);
+      const msg3 = greeting(undefined, []);
+
+      expect(msg.length).toBe(0);
+      expect(msg1.length).toBe(0);
+      expect(msg2.length).toBe(0);
+      expect(msg3.length).toBe(0);
+    });
+
+    test('should correct user ID and format @user mention', () => {
+      const id = `1234ef`; 
+      const msg = greeting(id, []);
+      const name = msg[0].text.text.match(/(<@)\w+>/g).pop();
+      expect(name).toBe('<@1234EF>');
+    });
   })
 });
